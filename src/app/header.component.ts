e555a63bbcfb4aab1d6f66b4d27e7a3b64c7316a
aniebietafia <aniebietafia87@gmail.com>
2025-05-20 21:49:09 +0100

    Add header component.
    Signed-off-by: aniebietafia <aniebietafia87@gmail.com>

---
+++ src/app/header.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+  title = 'UnravelDocs';
+  isLoggedIn = false;
+
+  constructor() {}
+
+  toggleLogin() {
+    this.isLoggedIn = !this.isLoggedIn;
+  }
+
+  getLoginStatus() {
+    return this.isLoggedIn ? 'Logout' : 'Login';
+  }
+}
