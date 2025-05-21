37578311c4cf38cef4c864f68213442982897b9b
aniebietafia <aniebietafia87@gmail.com>
2025-05-21 20:50:23 +0100

    Implement function to apply blur effect.
    Signed-off-by: aniebietafia <aniebietafia87@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from './header.component';
+import { NgIf, NgStyle } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [NgStyle, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'UnravelDocs';
+export class AppComponent implements OnInit {
+  protected percent = 0;
+  protected blur = 30;
+
+  get loadingOpacity() {
+    return this.map(this.percent, 0, 100, 1, 0);
+  }
+
+  ngOnInit(): void {
+    const interval = setInterval(() => {
+      if (this.percent < 100) {
+        this.percent++;
+        this.blur = this.map(this.percent, 0, 100, 30, 0);
+      } else {
+        clearInterval(interval);
+      }
+    }, 30);
+  }
+
+  map(
+    num: number,
+    in_min: number,
+    in_max: number,
+    out_min: number,
+    out_max: number
+  ) {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+  }
 }
